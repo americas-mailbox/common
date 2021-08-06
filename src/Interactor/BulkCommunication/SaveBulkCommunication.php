796ccb351b73b3a6a9168e2be895499f4933c67c
iampersistent <shank@zestic.com>
2021-08-05 22:08:46 -0700

    fixes to bulk communication command

--- src/Interactor/BulkCommunication/SaveBulkCommunication.php
+++ src/Interactor/BulkCommunication/SaveBulkCommunication.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace AMB\Interactor\BulkCommunication;
 
 use AMB\Entity\BulkCommunication;
+use AMB\Interactor\Db\BoolToSQL;
 use Doctrine\DBAL\Connection;
 use Exception;
 use IamPersistent\SimpleShop\Interactor\ObjectHasId;
@@ -63,6 +64,7 @@ final class SaveBulkCommunication
     private function prepDataForPersistence(BulkCommunication $bulkCommunication): array
     {
         return [
+            'completed'     => (new BoolToSQL())($bulkCommunication->isCompleted()),
             'email_body'    => $bulkCommunication->getEmailBody(),
             'scheduled_for' => $bulkCommunication->getScheduledFor()->toDateTimeString(),
             'send_options'  => json_encode($bulkCommunication->getSendOptions()),
