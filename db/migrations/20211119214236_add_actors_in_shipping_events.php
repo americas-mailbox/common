7a227dccf90e7a837e4bac56739bb8ac52b1b3a7
iampersistent <shank@zestic.com>
2021-11-19 22:26:15 -0600

    add migration for adding actors to shipping events

---
+++ db/migrations/20211119214236_add_actors_in_shipping_events.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddActorsInShippingEvents extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $this->table('shipping_events')
+            ->addColumn('created_by_admin_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('created_by_member_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('updated_by_admin_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('updated_by_member_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+            ])
+            ->update();
+    }
+}
