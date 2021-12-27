75e4412d59b4b003177fd29bb321efc939ebe052
Tarun Jangra <tarun@izap.in>
2021-12-27 05:18:30 -0800

    FindMembersByAttributres

---
+++ src/Interactor/Member/FindMemberByAttributes.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AMB\Interactor\Member;
+
+use AMB\Entity\Member;
+use AMB\Interactor\Db\HydrateMember;
+use Doctrine\DBAL\Connection;
+
+final class FindMemberByAttributes
+{
+    public function __construct(
+        private Connection $connection,
+        private HydrateMember $hydrateMember
+    ) {
+    }
+
+    public function find(array $attributes, string $operator = 'OR'): ?Member
+    {
+        $memberData = $this->gather($attributes, $operator);
+        if (empty($memberData)) {
+            return null;
+        }
+
+        return $this->hydrateMember->hydrate($memberData);
+    }
+
+    public function gather(array $attributes, string $operator)
+    {
+        $sql = $this->sql($attributes, $operator);
+        $statement = $this->connection->executeQuery($sql['statement'], $sql['params']);
+        $memberData = $statement->fetchAssociative();
+        if (empty($memberData)) {
+            return null;
+        }
+
+        return $memberData;
+    }
+
+    private function sql(array $attributes, string $operator): array
+    {
+        $whereCondition = implode(
+            $operator === 'OR' ? ' OR ' : ' AND ',
+            array_map(
+                function ($e) {
+                    return $e . '= ?';
+                },
+                array_keys($attributes)
+            )
+        );
+        $columnValues = array_values($attributes);
+        return [
+            'statement' => "SELECT * FROM member WHERE $whereCondition",
+            'params' => $columnValues
+        ];
+    }
+}
