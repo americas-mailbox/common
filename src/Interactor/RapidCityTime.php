cfb732953dcba77f1bbaa0c66b0bb6aa99841c88
iampersistent <shank@zestic.com>
2022-02-20 19:58:00 -0600

    Add UpcomingVehicleRenewalCommunication

--- src/Interactor/RapidCityTime.php
+++ src/Interactor/RapidCityTime.php
@@ -5,7 +5,7 @@ namespace AMB\Interactor;
 
 use Carbon\Carbon;
 
-final class RapidCityTime extends Carbon
+class RapidCityTime extends Carbon
 {
     public function __construct($time = null, $tz = null)
     {
