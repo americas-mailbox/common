0cabd1b7e05d2f38baa24c7e2f442180e1735003
iampersistent <shank@zestic.com>
2022-06-16 09:30:41 -0500

    move shipment history to common

---
+++ src/Interactor/Shipping/FormatAddressForTable.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace AMB\Interactor\Shipping;
+
+final class FormatAddressForTable
+{
+    public function __invoke(array $data): string
+    {
+        if ('pickup' === $data['delivery_group']) {
+            return 'Americas Mailbox, 514 Americas Way, Box Elder, SD';
+        }
+
+        $addressPieces = [];
+        if (!empty($data['address'])) {
+            $addressPieces[] = $data['address'];
+        }
+        if (!empty($data['suite'])) {
+            $addressPieces[] = $data['suite'];
+        }
+        if (!empty($data['city'])) {
+            $addressPieces[] = $data['city'];
+        }
+        if (!empty($data['state'])) {
+            $addressPieces[] = $data['state'];
+        }
+
+        return implode(', ', $addressPieces);
+    }
+}
---
+++ src/Interactor/Shipping/GatherShipmentHistoryForMember.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace AMB\Interactor\Shipping;
+
+use App\Entity\Paginate;
+use AMB\SQLBuilder\PaginateToSQL;
+use Doctrine\DBAL\Connection;
+
+final class GatherShipmentHistoryForMember
+{
+    public function __construct(
+        private Connection $connection,
+    ) {
+    }
+
+    public function gather(int $memberId, Paginate $paginate = null)
+    {
+        return $this->connection->fetchAllAssociative($this->sql($memberId, $paginate));
+    }
+
+    private function sql($memberId, Paginate $paginate = null): string
+    {
+        $paginateSql = (new PaginateToSQL)($paginate);
+
+        return <<<SQL
+SELECT 
+s.id,
+s.date,
+a.address,
+a.suite,
+a.city,
+a.state,
+dc.name AS carrierName,
+d.tracking_number as trackingNumber,
+le.debit AS charge
+FROM shipments s
+LEFT JOIN addresses a ON s.address_id = a.id
+LEFT JOIN deliveries d ON s.delivery_id = d.id 
+LEFT JOIN delivery_carriers dc ON d.carrier_id = dc.id
+LEFT JOIN members m ON s.member_id = m.member_id
+LEFT JOIN accounts ac ON m.account_id = ac.id
+LEFT JOIN ledgers l on ac.ledger_id = l.id
+LEFT JOIN ledger_entries le ON l.id = le.ledger_id AND s.date = le.date
+WHERE s.member_id = $memberId
+AND fulfilled = 1
+ORDER BY date DESC
+$paginateSql
+SQL;
+    }
+}
