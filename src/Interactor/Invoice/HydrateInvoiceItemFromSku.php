505632ade0b8c3ec27f89e79b245a6a0e37ea66b
iampersistent <shank@zestic.com>
2022-08-21 18:21:23 -0500

    move HydrateInvoiceItemFromSku to common

---
+++ src/Interactor/Invoice/HydrateInvoiceItemFromSku.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace AMB\Interactor\Invoice;
+
+use IamPersistent\SimpleShop\Entity\InvoiceItem;
+use IamPersistent\SimpleShop\Entity\Product;
+
+final class HydrateInvoiceItemFromSku
+{
+    public function __invoke(InvoiceItem $item, Product $sku, $quantity = 1)
+    {
+        $amount = $sku->getPrice();
+        $total = $amount->multiply($quantity);
+
+        $item
+            ->setAmount($amount)
+            ->setDescription($sku->getDescription())
+            ->setQuantity($quantity)
+            ->setProduct($sku)
+            ->setIsTaxable($sku->isTaxable())
+            ->setTotalAmount($total);
+    }
+}
