20e6fb29151c79e96b19334a19db1c7d8d6c9f15
iampersistent <shank@zestic.com>
2023-04-14 21:32:52 -0500

    member refactoring

--- src/Entity/MemberStatus.php
+++ src/Entity/MemberStatus.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace AMB\Entity;
 
-use MyCLabs\Enum\Enum;
-
-final class MemberStatus extends Enum
+enum MemberStatus: string
 {
-  // members table have column active.
-  const ACTIVE = 1; // user can login.
-  const CLOSED = 0; // user can not login.
-  const UNVERIFIED = 2; // New member can not login.
-  const UNPAID = 3; // New member got registered from frontend without making any payment.
+    case ACTIVE = 'ACTIVE';
+    case CLOSED = 'CLOSED'; // user cannot log in
+    case SUSPENDED = 'SUSPENDED';
+    case UNVERIFIED = 'UNVERIFIED';
 }
--- src/Entity/Membership.php
+++ src/Entity/Membership.php
@@ -3,33 +3,188 @@ declare(strict_types=1);
 
 namespace AMB\Entity;
 
+use AMB\Entity\Account\Notifications;
+use AMB\Entity\Member\Plan as MemberPlan;
+use AMB\Interactor\RapidCityTime;
+use Carbon\Carbon;
+use IamPersistent\Ledger\Entity\Ledger;
+
 final class Membership
 {
-    /** @var int */
-    private $id;
-    /** @var string */
-    private $pmb;
+    private Account $account;
+    private ?string $comment;
+    private int $id;
+    private ?MemberPlan $memberPlan = null;
+    private string|null $pmb = null;
+    private Carbon|null $renewDate;
+    private ?RapidCityTime $returnToSenderDate = null;
+    private string $shippingInstructions = '';
+    private Carbon|null $startDate = null;
+
+    public function getAccount(): Account
+    {
+        return $this->account;
+    }
+
+    public function setAccount(Account $account): Member
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    public function getComment(): string
+    {
+        return (string) $this->comment;
+    }
+
+    public function setComment(?string $comment): Member
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
 
     public function getId(): int
     {
         return $this->id;
     }
 
-    public function setId(int $id): Membership
+    public function setId(int $id): Member
     {
         $this->id = $id;
 
         return $this;
     }
 
-    public function getPmb(): string
+    public function getLedger(): Ledger
+    {
+        return $this->getAccount()->getLedger();
+    }
+
+    public function getMemberPlan(): ?MemberPlan
+    {
+        return $this->memberPlan;
+    }
+
+    public function setMemberPlan(?MemberPlan $plan): Member
+    {
+        $this->memberPlan = $plan;
+
+        return $this;
+    }
+
+    public function getMemberStatus(): MemberStatus
+    {
+        return $this->active;
+    }
+
+    public function getNotifications(): Notifications
+    {
+        return $this->account->getNotifications();
+    }
+
+    public function getPIN(): ?string
+    {
+        return $this->pin;
+    }
+
+    public function setPIN(?string $pin): Member
+    {
+        $this->pin = $pin;
+
+        return $this;
+    }
+
+    public function getPlan(): ?MemberPlan
+    {
+        return $this->memberPlan;
+    }
+
+    public function setPlan(?MemberPlan $memberPlan): Member
+    {
+        $this->memberPlan = $memberPlan;
+
+        return $this;
+    }
+
+    public function getPMB(): string|null
     {
         return $this->pmb;
     }
 
-    public function setPmb(string $pmb): Membership
+    public function setPMB(string|null $pmb): Member
     {
         $this->pmb = $pmb;
+
+        return $this;
+    }
+
+    public function getRenewDate(): ?Carbon
+    {
+        return $this->renewDate;
+    }
+
+    public function setRenewDate(?Carbon $renewDate): Member
+    {
+        $this->renewDate = $renewDate;
+
+        return $this;
+    }
+
+    public function getReturnToSenderDate(): ?RapidCityTime
+    {
+        return $this->returnToSenderDate;
+    }
+
+    public function setReturnToSenderDate(?RapidCityTime $returnToSenderDate): Member
+    {
+        $this->returnToSenderDate = $returnToSenderDate;
+
+        return $this;
+    }
+
+    public function getShippingInstructions(): string
+    {
+        return (string) $this->shippingInstructions;
+    }
+
+    public function setShippingInstructions(?string $shippingInstructions): Member
+    {
+        $this->shippingInstructions = $shippingInstructions;
+
+        return $this;
+    }
+
+    public function getStartDate(): Carbon
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(Carbon $startDate): Member
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        if ($this->suspended) {
+            return 'Suspended';
+        }
+
+        return ucfirst(strtolower($this->active->getKey()));
+    }
+
+    public function useNewDashboard(): bool
+    {
+        return $this->useNewDashboard;
+    }
+
+    public function setUseNewDashboard(bool $useNewDashboard): Member
+    {
+        $this->useNewDashboard = $useNewDashboard;
 
         return $this;
     }
