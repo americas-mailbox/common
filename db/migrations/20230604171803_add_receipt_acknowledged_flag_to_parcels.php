444208094304816e33a211cf65aa78ba03d5348e
iampersistent <shank@zestic.com>
2023-06-04 12:19:25 -0500

    add migration

---
+++ db/migrations/20230604171803_add_receipt_acknowledged_flag_to_parcels.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddReceiptAcknowledgedFlagToParcels extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table('parcels')
+            ->addColumn('receipt_acknowledged', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'default' => 0])
+            ->update();
+    }
+}
